a8c7cead14b1c78875ae2cb9318eef39af594fcd
Roberto Lomelí <roberto@debian-roberto.udgnet>
2019-11-08 10:35:58 -0600

    Finish restapi

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $items = Item::all();
+        return response()->json(["items" => $items],200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $item = new Item();
+        $item->name = $request->name;;
+        $item->stock = $request->stock;
+        $item->save();
+        return response()->json(["message"=>"Success, item created!"],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        
+            $item = Item::find($id);
+            if ($item != null){
+                return response()->json(["item" => $item],200);
+            }else{
+                return response()->json(["message" => "Item not found"],404);
+            }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $item = Item::find($id);
+        $item->name = $request->name;
+        $item->stock = $request->stock;
+        $item->save();
+
+        return response()->json(["message"=>"Item was updated"],200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Item::destroy($id);
+    }
+}
